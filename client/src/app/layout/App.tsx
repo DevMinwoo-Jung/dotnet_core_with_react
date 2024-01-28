1ff7ec9534ccd84949ed28023e4b49af1f134f73
DEVMINWOO-JUNG <applejung312@gmail.com>
2024-01-28 21:18:55 +0900

    add darkMode

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,17 +1,35 @@
-import { Container, CssBaseline } from "@mui/material";
+import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import Catalog from "../../features/catalog/Catalog";
 import Header from "./Header";
+import { useEffect, useState } from "react";
 
 function App() {
 
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? 'dark': 'light';
+
+  const theme = createTheme({
+    palette: {
+      mode: paletteType
+    },
+  });
+
+  const changeTheme = () => {
+    setDarkMode((prev)=> !prev);
+  }
+
+  useEffect(()=> {
+    setDarkMode(darkMode);
+  }, [darkMode])
+
   return (
-    <>
+    <ThemeProvider theme={theme} >
       <CssBaseline/>
-      <Header/>
+      <Header setTheme={changeTheme}/>
       <Container>
         <Catalog/>      
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,12 +1,19 @@
 import { AppBar, Toolbar, Typography } from '@mui/material'
+import CustomizedSwitches from '../components/ThemeBtn'
 
-export default function Header() {
+export type HeaderType = {
+  setTheme: ()=> void;
+}
+
+export default function Header({setTheme}:HeaderType) {
+  
   return (
     <AppBar position='static' sx={{mb: 4}}>
       <Toolbar>
         <Typography variant='h6'>
           WooPang
         </Typography>
+        <CustomizedSwitches setTheme={setTheme}/>
       </Toolbar>
     </AppBar>
   )
