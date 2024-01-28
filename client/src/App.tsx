3af705b63faf1212201e489c6dd1e12028ae6030
DEVMINWOO-JUNG <applejung312@gmail.com>
2024-01-28 11:17:24 +0900

    useSate

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+
+const dummy = [
+  { name: '물건1', price: 100.00},
+  { name: '물건2', price: 200.00},
+]
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState([...dummy]);
+
+  function addProducts(){
+    setProducts(prevState => [...prevState, {
+      name: `products${Math.random()}`,
+      price: Math.random() * 1000 
+    }])
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 style={{color: 'blue'}}>Vite + React</h1>
+      <ul>
+        {
+          products.map((ele, index)=>{
+            return (
+                <li key={index}>{ele.name}{ele.price}</li>
+            )
+          })
+        }
+      </ul>
+      <button onClick={addProducts}>Add Product</button>
     </>
   )
 }
