4ab3486ac7c7fe31e7443f1e74b1ae06098d6d22
DEVMINWOO-JUNG <applejung312@gmail.com>
2024-02-14 21:42:31 +0900

    올해 목표는 이직

--- client/src/features/basket/Basket.tsx
+++ client/src/features/basket/Basket.tsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react"
 import { Basket } from "../../app/models/basket";
 import agent from "../../app/api/agent";
 import Loading from "../../app/layout/Loading";
-import { Typography } from "@mui/material";
+import { IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import { Delete } from "@mui/icons-material";
 
 export default function Basket() {
 
@@ -21,6 +22,38 @@ export default function Basket() {
   if(!basket) return <Typography variant="h3">Your basket it empoty</Typography>
 
   return (
-    <div>Buyer Id = {basket.buyerId}</div>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Product</TableCell>
+            <TableCell align="right">Price</TableCell>
+            <TableCell align="right">Qunatity</TableCell>
+            <TableCell align="right">Subtotal</TableCell>
+            <TableCell align="right">)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {basket.items.map((item) => (
+            <TableRow
+              key={item.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {item.name}
+              </TableCell>
+              <TableCell align="right">{(item.price / 100).toFixed(2)}원</TableCell>
+              <TableCell align="right">{item.quantity}</TableCell>
+              <TableCell align="right">{item.price * item.quantity}</TableCell>
+              <TableCell align="right">
+                <IconButton color="error">
+                  <Delete/>
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   )
 }
